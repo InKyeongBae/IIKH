6fde20733ef213350b03194b284371dbe91c014b
InKyeong <hwang0809@cau.ac.kr>
2020-10-07 22:11:10 +0900

    date->8자리 한 번에 입력 string형으로

--- IIKH/date.h
+++ IIKH/date.h
@@ -6,25 +6,21 @@ using namespace std;
 class Date {
 
 private:
-    int year, month, day;
-    bool anniversary = 0;
+    string full_date;
+    string anniversary = "";
     map <string, int> grocery_list;
 
 public:
 
-    void set_date(int y, int m, int d, bool a, string g_name, int g_amt);
+    void set_date(string d, string a, string g_name, int g_amt);
 
-    int get_year() { return year; }
-    int get_month() { return month; }
-    int get_day() { return day; }
-    bool get_anniversary() { return anniversary; }
+    string get_full_date() { return full_date; }
+    string get_anniversary() { return anniversary; }
     //컴파일 확인용 함수
     void show_date();
 
-    Date(int y, int m, int d, bool a, string g_name, int g_amt) {
-        year = y;
-        month = m;
-        day = d;
+    Date(string d, string a, string g_name, int g_amt) {
+        full_date = d;
         anniversary = a;
         grocery_list.insert(make_pair(g_name, g_amt));
     };
