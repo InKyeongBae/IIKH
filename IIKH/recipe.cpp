18af283b1518cc46948eefc8eed10a8c371e7066
seungahkim <safg0421@cau.ac.kr>
2020-10-07 21:42:36 +0900

    Plan/Meal/Recipe 헤더파일구현

---
+++ IIKH/recipe.cpp
@@ -0,0 +1,14 @@
+#include <map>
+#include <string>
+using namespace std;
+class Recipe
+{
+private:
+	string recipe_name;
+	map<string, int> ingrediants;
+	string description;
+	int time;
+public:
+
+
+};
